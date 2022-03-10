9e504d305c099b7d610ae86918cfe683809d5d5a
labibahmed10 <labib.ahmed.372@gmail.com>
2022-03-10 20:49:15 +0600

    status text length done

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ const reportPost = (id) => {
 };
 
 const displayContent = (text) => {
-  return text.length < 30 ? text : text.slice(0, 25) + "<span class='fw-bold'>... read more</span>";
+  return text.length < 30 ? text : text.slice(0, 28) + "<span class='fw-bold'>... read more</span>";
 };
 
 const switchTab = (id) => {
@@ -91,7 +91,6 @@ const createPost = (post) => {
                   <button class="post__button">
                     <i class="fa-solid fa-comment"></i>
                   </button>
-                  
 
                   <div class="post__indicators"></div>
 
@@ -118,9 +117,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0]?.user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0]?.text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
